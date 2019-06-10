c9b7b1fd0046da8137209d88459a8ac57b64b045
Deepesh-Kumar <deepeshkumar1991@gmail.com>
2019-06-09 18:04:23 -0700

    new pod change

--- scripts/template.py
+++ scripts/template.py
@@ -23,18 +23,12 @@ class push_device_template:
     	q1 = json.dumps(payload1)
     	response = s.post(uri, auth=HTTPBasicAuth(self.username, self.password), data= q1, headers=headers1, verify=False)
     	cook = response.cookies	
-    	urv = 'https://' + self.IP + '/dataservice/template/device/config/attachfeature'
+    	urv = 'https://' + self.IP + '/dataservice/template/device/config/attachcli'
     	headers = {'Content-Type': 'application/json'}
-    	payload = {"deviceTemplateList":{"deviceTemplateList":[{"templateId":"6b985229-0ff8-4cde-b5e3-8d05a518007d",
-    				"device":[{"csv-status":"complete","csv-deviceId":"0f500217-8dac-45d2-14a7-c19120a25af7",
-    				"csv-deviceIP":"1.10.1.2","csv-host-name":"dc10-vedge2","/0/ge0/0/interface/if-name":"ge0/0",
-    				"/512/ge0/6/interface/if-name":"ge0/6","//system/site-id":"10","/0/ge0/1/interface/if-name":"ge0/1",
-    				"/512/ge0/6/interface/ip/address":"192.168.150.10/24","//system/host-name":"dc10-vedge2",
-    				"//system/system-ip":"1.10.1.2","/10/ge0/2/interface/if-name":"ge0/2","/512/eth0/interface/if-name":"eth0",
-    				"/512/vpn-instance/ip/route/0.0.0.0/0/prefix":"0.0.0.0/0","/10/ge0/2/interface/ip/address":"10.10.1.3/24","/0/ge0/1/interface/ip/address":"10.10.2.2/30",
-    				"/512/vpn-instance/ip/route/0.0.0.0/0/next-hop/192.168.150.1/address":"192.168.150.1",
-    				"/0/vpn-instance/ip/route/0.0.0.0/0/prefix":"0.0.0.0/0","/0/vpn-instance/ip/route/0.0.0.0/0/next-hop/10.10.2.1/address":"10.10.2.1",
-    				"/0/vpn-instance/ip/route/0.0.0.0/0/next-hop/19.10.2.1/address":"19.10.2.1","csv-templateId":"6b985229-0ff8-4cde-b5e3-8d05a518007d"}],"isEdited":false,"isMasterEdited":false}]}
+    	payload = {"deviceTemplateList":[{"templateId":"42d40990-8c65-43f7-ba42-1d91fbc8fa3c",
+                    "device":[{"csv-status":"complete","csv-deviceId":"0f500217-8dac-45d2-14a7-c19120a25af7",
+                    "csv-deviceIP":"1.10.1.2","csv-host-name":"dc10-vedge2","csv-templateId":"42d40990-8c65-43f7-ba42-1d91fbc8fa3c"}],
+                    "isEdited":"false"}]}
     	q = json.dumps(payload)
     	response1 = s.post(urv, auth=HTTPBasicAuth(self.username, self.password), data= q, headers=headers, cookies=cook, verify=False)
     	print (response1, response1.content)
