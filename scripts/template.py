0a77e44f69691b36b6a498f0c9abf640327c2843
Deepesh-Kumar <deepeshkumar1991@gmail.com>
2019-06-11 20:26:13 -0700

    pod changes

--- scripts/template.py
+++ scripts/template.py
@@ -6,7 +6,7 @@ from requests.auth import HTTPBasicAuth
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  
 import sys 
-s = requests.session()  
+session = requests.session()  
 
 class push_device_template:
 
@@ -15,24 +15,19 @@ class push_device_template:
         self.username = username
         self.password = password
 
-
     def push_template(self):
-    	uri =  'https://' + self.IP + '/j_security_check'
-    	headers1 = {'Content-Type': 'application/x-www-form-urlencoded'}
-    	payload1 = {}
-    	q1 = json.dumps(payload1)
-    	response = s.post(uri, auth=HTTPBasicAuth(self.username, self.password), data= q1, headers=headers1, verify=False)
-    	cook = response.cookies	
-    	urv = 'https://' + self.IP + '/dataservice/template/device/config/attachcli'
-    	headers = {'Content-Type': 'application/json'}
-    	payload = {"deviceTemplateList":[{"templateId":"42d40990-8c65-43f7-ba42-1d91fbc8fa3c",
-                    "device":[{"csv-status":"complete","csv-deviceId":"0f500217-8dac-45d2-14a7-c19120a25af7",
-                    "csv-deviceIP":"1.10.1.2","csv-host-name":"dc10-vedge2","csv-templateId":"42d40990-8c65-43f7-ba42-1d91fbc8fa3c"}],
-                    "isEdited":"false"}]}
-    	q = json.dumps(payload)
-    	response1 = s.post(urv, auth=HTTPBasicAuth(self.username, self.password), data= q, headers=headers, cookies=cook, verify=False)
-    	print (response1, response1.content)
-
+        api_call_first =  'https://' + self.IP + '/j_security_check'
+        headers1 = {'Content-Type': 'application/x-www-form-urlencoded'}
+        payload1 = {}
+        payload_js = json.dumps(payload1)
+        response_js = session.post(api_call_first, auth=HTTPBasicAuth(self.username, self.password), data= payload_js, headers=headers1, verify=False)
+        cookie = response_js.cookies 
+        api_call = 'https://' + self.IP + '/dataservice/template/device/config/attachcli'
+        headers = {'Content-Type': 'application/json'}
+        payload = {"deviceTemplateList":[{"templateId":"42d40990-8c65-43f7-ba42-1d91fbc8fa3c","device":[{"csv-status":"complete","csv-deviceId":"0f500217-8dac-45d2-14a7-c19120a25af7","csv-deviceIP":"1.10.1.2","csv-host-name":"dc10-vedge2","csv-templateId":"42d40990-8c65-43f7-ba42-1d91fbc8fa3c"}],"isEdited":"false"}]}
+        data = json.dumps(payload)
+        response_js_final = session.post(api_call, auth=HTTPBasicAuth(self.username, self.password), data= data, headers=headers, cookies=cookie, verify=False)
+        print (response_js_final, response_js_final.content)
 
 
 
